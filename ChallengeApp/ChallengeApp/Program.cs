abd0dc57a4e3458fb40d1f7fd5d05cf870a32b48
Hubert <hbryja@gmail.com>
2023-08-18 21:48:41 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+string name = "Ewa";
+string sex = "kobieta";
+int age = 33;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33 && sex == "kobieta")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (name == "Ewa" &&  sex == "kobieta" && age > 18)
+{
+    Console.WriteLine("Ewa pełnoletnia kobieta");
+}
