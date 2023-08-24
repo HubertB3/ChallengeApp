97243c735bd2338d6555af29890e7ca9673c36ed
Hubert <hbryja@gmail.com>
2023-08-24 20:20:56 +0200

    Dzień 5- zadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,22 +1,34 @@
-﻿using System.ComponentModel.Design;
+﻿int number = 4594;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+int[] counter = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-string name = "Ewa";
-string sex = "kobieta";
-int age = 33;
+foreach (char letter in letters)
+{
+    if (letter == '0')
+    {counter[0]++;}
+    else if (letter == '1')
+    {counter[1]++;}
+    else if (letter == '2')
+    {counter[2]++;}
+    else if (letter == '3')
+    {counter[3]++;    }
+    else if (letter == '4')
+    {counter[4]++;}
+    else if (letter == '5')
+    {counter[5]++;}
+    else if (letter == '6')
+    {counter[6]++;}
+    else if (letter == '7')
+    {counter[7]++;}
+    else if (letter == '8')
+    {counter[8]++;}
+    else if (letter == '9')
+    {counter[9]++;}
+}
 
-if (sex == "kobieta" && age < 30)
+
+for (int i = 0; i < counter.Length; i++)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33 && sex == "kobieta")
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (sex != "kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else if (name == "Ewa" &&  sex == "kobieta" && age > 18)
-{
-    Console.WriteLine("Ewa pełnoletnia kobieta");
+    Console.WriteLine(i + " ==> " + counter[i]);
 }
